c724935933c28abe68dcb09c35433dc73058c867
Anand Raj <60174790+raj9anand@users.noreply.github.com>
2026-01-06 12:43:09 +0530

    Add variable examples in 2.variable.tcl
    Added examples of variable assignment and indexing in Tcl.

--- 2.variable.tcl
+++ 2.variable.tcl
@@ -1,1 +1,21 @@
+set abc "123"
+puts $abc
+
+set def(1) 4567; 
+puts $def(1);
+
+set def(test) 4567;
+
+puts $def(test);
+#puts $def ##this will give u error
+
+###An index doesn’t have to be an integer
+
+
+
+set x [set y 100];  #This will set the value of x to 100
+puts $x
+
+set x {[set y 100]}; #This will set the value of x to [set y 100]
+puts $x
 
