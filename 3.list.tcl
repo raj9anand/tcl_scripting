37e6ea4f2535e8e7cd9b959efb85e27434c29823
Anand Raj <60174790+raj9anand@users.noreply.github.com>
2026-01-06 12:54:36 +0530

    Enhance Tcl script with list operations examples
    Added examples of creating and manipulating lists in Tcl.

--- 3.list.tcl
+++ 3.list.tcl
@@ -1,1 +1,20 @@
+##create list
+set gates [list AND OR NOT NAND NOR ]
+puts $gates
 
+#set gates {{AND} {OR} {NOT} {NAND} {NOR}}
+#puts $gates
+
+#set gates [split "AND.OR.NOT.NAND.NOR"]
+#puts $gates
+
+##above are three different way to create list
+
+
+set gates [lappend gates XOR] ;   # To add another item to a list, use lappend
+
+puts $gates;
+
+puts [lsearch $gates NOT];  # To search within a list, use lsearch . This returns the matching indices of the list
+
+puts [lsearch $gates XNOR]; # Returns -1, since there is no match found
